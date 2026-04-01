747dc8436b4520e077daa482724e3620d231ac60
leekj9633 <123793444+leekj9633@users.noreply.github.com>
2026-04-01 17:19:21 +0900

    [Bronze V] Title: ??!, Time: 164 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String id = scan.next();
+
+        System.out.println(id + "??!");
+    }
+    
+}
